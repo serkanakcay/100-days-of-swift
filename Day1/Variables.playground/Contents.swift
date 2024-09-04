df31fd068edbc283719c7221396c8fc8516fbe5b
Serkan Akçay <serkanakcayyy@gmail.com>
2024-09-04 14:15:44 +0300

    Day1

---
+++ Day1/Variables.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+// Mark: Variables
+
+var str = "Hello Playground"
+str = "Goodbye"
+
+var age = 38
+var population = 8_000_000
+
+var str1 = """
+This goes
+over multiple
+Lines
+"""
+
+var str2 = """
+This goes \ 
+over Multiple \
+lines
+"""
+
+
+// Mark: Doubles and Booleans
+
+var pi = 3.141
+
+
+// Mark: String
+
+var score = 85
+print("Your Score was \(score)")
+let taylor = "Swift"
+
+
+
+// MARK : TYPE ANNOTATIONS
+
+let strOne = "Hello, playground"
+let album : String = "Reputation"
+let year : Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+
+
